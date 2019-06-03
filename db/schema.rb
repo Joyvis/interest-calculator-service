26d66156b5ca057563446fa14505e6ba78fbe625
Joyvis Santana <jjs276@gmail.com>
2019-06-02 23:06:31 -0300

    model validations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_012314) do
+ActiveRecord::Schema.define(version: 2019_06_03_020016) do
 
   create_table "installments", force: :cascade do |t|
     t.float "present_value", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_012314) do
     t.string "uuid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "installment_value", null: false
   end
 
 end
