7821548b93f405ef8d83c0cba553388a0ba8da52
Joyvis Santana <jjs276@gmail.com>
2019-06-02 23:06:31 -0300

    model validations and tests

--- spec/models/installment_spec.rb
+++ spec/models/installment_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Installment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validating' do
+    context 'presence' do
+      it { is_expected.to validate_presence_of(:present_value) }
+      it { is_expected.to validate_presence_of(:number_of_installments) }
+      it { is_expected.to validate_presence_of(:monthly_interest_rate) }
+      it { is_expected.to validate_presence_of(:installment_value) }
+      it { is_expected.to validate_presence_of(:uuid) }
+    end
+  end
 end
