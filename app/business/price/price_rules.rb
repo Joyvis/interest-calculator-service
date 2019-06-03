b78ef3f8460b86660c427661298e119586180742
Joyvis Santana <jjs276@gmail.com>
2019-06-03 05:33:11 -0300

    rucoping

--- app/business/price/price_rules.rb
+++ app/business/price/price_rules.rb
@@ -6,8 +6,8 @@ class PriceRules
   def self.calculate(installment_attributes)
     installment = Installment.new(installment_attributes)
     installment.valid?
-
     return installment unless valid_installment?(installment)
+
     new(installment).calculate
   end
 
@@ -21,22 +21,22 @@ class PriceRules
     installment
   end
 
+  def self.valid_installment?(installment)
+    required_fields = %i[present_value number_of_installments
+                         monthly_interest_rate]
+    required_fields.none? do |field|
+      installment.errors.details.include?(field)
+    end
+  end
+
   private
 
   def calculate_installment
     calc = (1 + installment.monthly_interest_rate)
-    calc = calc ** installment.number_of_installments
-    calc = 1 - ( 1 / calc )
+    calc **= installment.number_of_installments
+    calc = 1 - (1 / calc)
     calc = installment.monthly_interest_rate / calc
 
     (installment.present_value * calc).round(2)
-  end
-
-  def self.valid_installment?(installment)
-    required_fields = [:present_value, :number_of_installments,
-                       :monthly_interest_rate]
-    !required_fields.any? do |field|
-      installment.errors.details.include?(field)
-    end
   end
 end
--- spec/business/price/price_rules_spec.rb
+++ spec/business/price/price_rules_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe PriceRules, type: :business do
